f78ecc656df7ea83e84f9782773a7aea106db763
Artem Govorov <artem.govorov@gmail.com>
2017-04-19 11:02:41 +1000

    Intial commit

---
+++ src/a/b/c/router.test.js
@@ -0,0 +1,17 @@
+import {history as hrt} from './../../../stores/Router'
+import createRouterContext from 'react-router-test-context'
+import { mount } from 'enzyme';
+import React from 'react';
+const context = createRouterContext({history: hrt});
+
+it('pressing BtnsGroup.ActivityFlowBtns should change route to desired flow', async () => {
+  wrapper = mount(<Layout />, { context });
+
+  const CreateContentBtn = wrapper.find('.Btn').at(1);
+  CreateContentBtn.simulate('click');
+
+  function assertCorrectPath(ev) {
+    expect(ev.pathname).toEqual('/content'); //running `npm test` works well
+  }
+  await wrapper.context().router.history.subscribe(assertCorrectPath);
+});
---
+++ src/stores/Router.js
@@ -0,0 +1,10 @@
+//router store
+import { RouterStore, syncHistoryWithStore } from 'mobx-react-router';
+
+import createBrowserHistory from 'history/createBrowserHistory';
+const browserHistory = createBrowserHistory();
+
+const router = new RouterStore();
+export const history = syncHistoryWithStore(browserHistory, router);
+
+export default router;
